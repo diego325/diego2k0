386caa0624317ec144d7c1f13bd2cc1eba9f8f53
diego325 <80598034+diego325@users.noreply.github.com>
2021-03-18 14:20:36 -0300

    Add files via upload

---
+++ src/outros.js
@@ -0,0 +1,41 @@
+const iklan = (.) => { 
+	return `           
+╔══✪〘 *OUTROS* 〙✪══
+║
+╠➥ *${.}linkgp*
+╠➥ *${.}simih [1/0]*
+╠➥ *${.}marcar*
+╠➥ *${.}add [@]*
+╠➥ *${.}banir [@]*
+╠➥ *${.}promover [@]*
+╠➥ *${.}rebaixar*
+╠➥ *${.}admins*
+╠➥ *${.}marcar2*
+╠➥ *${.}bc [texto]* (ele faz uma ™)
+╠➥ *${.}marcar3*
+╠➥ *${.}bloqueados*
+╠➥ *${.}bloquear [@]*
+╠➥ *${.}desbloquear [@]*
+╠➥ *${.}limpar*
+╠➥ *${.}bc [ *texto* ]*
+╠➥ *${.}bemvindo [1/0]*
+╠➥ *${.}clonar [@]*
+╠➥ *${.}help1*
+╠➥ *${.}dono*
+╠➥ *${.}owner*
+╠➥ *${.}tts [texto]*
+╠➥ *${.}setnome*
+╠➥ *${.}termux*
+╠➥ *${.}setfoto*
+╠➥ *${.}grupoinfo*
+╠➥ *${.}ytmp4*
+╠➥ *${.}bomdia*
+╠➥ *${.}boanoite*
+╠➸ *${.}marcar*
+╠➸ *${.}marcar2*
+╠➸ *${.}marcar3*
+║
+╚═〘  *DARKBOT* 〙
+`
+}
+exports.iklan = iklan
---
+++ src/vipmenu.js
@@ -0,0 +1,25 @@
+const vipmenu = (.) => { 
+	return `            
+	
+╔══✪〘 USUÁRIO VIP 〙✪══
+║
+╠➸ *${.}dado*
+╠➸ *${.}cekvip*
+╠➸ *${.}premiumlist*
+╠➸ *${.}delete*
+╠➸ *${.}modapk*
+╠➸ *${.}indo10*
+╠➸ *${.}daftarvip [para virar Premium]*
+╠➸ *${.}qrcode*
+╠➥ *${.}chentai*
+╠➥ *${.}gcpf*
+╠➥ *${.}gbin*
+╠➥ *${.}pack*
+╠➥ *${.}destrava*
+╠➥ *${.}gpessoa*
+║
+║
+╚══✪〘  DIEGO BOT 〙✪══
+`
+}
+exports.vipmenu = vipmenu
